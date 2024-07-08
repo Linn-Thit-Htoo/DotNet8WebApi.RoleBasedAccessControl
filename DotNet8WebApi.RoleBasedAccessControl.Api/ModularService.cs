b72dc31c19185d8fc8fc5d00c503cb39c61eaa54
Linn Thit Htoo <lth1212001@gmail.com>
2024-07-08 23:21:10 +0630

    Format Code

--- DotNet8WebApi.RoleBasedAccessControl.Api/ModularService.cs
+++ DotNet8WebApi.RoleBasedAccessControl.Api/ModularService.cs
@@ -2,52 +2,62 @@
 using DotNet8WebApi.RoleBasedAccessControl.DbService.AppDbContexts;
 using DotNet8WebApi.RoleBasedAccessControl.Repositories.Features.Auth;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
 
-namespace DotNet8WebApi.RoleBasedAccessControl.Api
+namespace DotNet8WebApi.RoleBasedAccessControl.Api;
+
+public static class ModularService
 {
-    public static class ModularService
+    public static IServiceCollection AddFeatures(
+        this IServiceCollection services,
+        WebApplicationBuilder builder
+    )
     {
-        public static IServiceCollection AddFeatures(this IServiceCollection services, WebApplicationBuilder builder)
-        {
-            return services.AddDbContextService(builder)
-                .AddRepositoryService()
-                .AddBusinessLogicService()
-                .AddJsonService();
-        }
+        return services
+            .AddDbContextService(builder)
+            .AddRepositoryService()
+            .AddBusinessLogicService()
+            .AddJsonService();
+    }
 
-        private static IServiceCollection AddDbContextService(this IServiceCollection services, WebApplicationBuilder builder)
-        {
-            return builder.Services.AddDbContext<AppDbContext>(opt =>
+    private static IServiceCollection AddDbContextService(
+        this IServiceCollection services,
+        WebApplicationBuilder builder
+    )
+    {
+        return builder.Services.AddDbContext<AppDbContext>(
+            opt =>
             {
                 opt.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
                 opt.UseSqlServer(builder.Configuration.GetConnectionString("DbConnection"));
-            }, ServiceLifetime.Transient);
-        }
+            },
+            ServiceLifetime.Transient
+        );
+    }
 
-        private static IServiceCollection AddRepositoryService(this IServiceCollection services)
-        {
-            return services.AddScoped<IAuthRepository, AuthRepository>();
-        }
+    private static IServiceCollection AddRepositoryService(this IServiceCollection services)
+    {
+        return services.AddScoped<IAuthRepository, AuthRepository>();
+    }
 
-        private static IServiceCollection AddBusinessLogicService(this IServiceCollection services)
-        {
-            return services.AddScoped<BL_Auth>();
-        }
+    private static IServiceCollection AddBusinessLogicService(this IServiceCollection services)
+    {
+        return services.AddScoped<BL_Auth>();
+    }
 
-        //private static IServiceCollection AddAuthenticationService(this IServiceCollection services)
-        //{
+    //private static IServiceCollection AddAuthenticationService(this IServiceCollection services)
+    //{
 
-        //}
+    //}
 
-        private static IServiceCollection AddJsonService(this IServiceCollection services)
-        {
-            services.AddControllers().AddJsonOptions(opt =>
+    private static IServiceCollection AddJsonService(this IServiceCollection services)
+    {
+        services
+            .AddControllers()
+            .AddJsonOptions(opt =>
             {
                 opt.JsonSerializerOptions.PropertyNamingPolicy = null;
             });
 
-            return services;
-        }
+        return services;
     }
 }
