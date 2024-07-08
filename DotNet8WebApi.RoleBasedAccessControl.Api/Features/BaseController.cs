0c34230ec502ef451349918930cb6eac03c72df9
Linn Thit Htoo <lth1212001@gmail.com>
2024-07-08 23:20:55 +0630

    Format Code

--- DotNet8WebApi.RoleBasedAccessControl.Api/Features/BaseController.cs
+++ DotNet8WebApi.RoleBasedAccessControl.Api/Features/BaseController.cs
@@ -1,21 +1,19 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 
-namespace DotNet8WebApi.RoleBasedAccessControl.Api.Features
-{
-    [Route("api/[controller]")]
-    [ApiController]
-    public class BaseController : ControllerBase
-    {
-        protected IActionResult Content(object obj)
-        {
-            return Content(JsonConvert.SerializeObject(obj), "application/json");
-        }
+namespace DotNet8WebApi.RoleBasedAccessControl.Api.Features;
 
-        protected IActionResult InternalServerError(Exception ex)
-        {
-            return StatusCode(500, ex.Message);
-        }
+[Route("api/[controller]")]
+[ApiController]
+public class BaseController : ControllerBase
+{
+    protected IActionResult Content(object obj)
+    {
+        return Content(JsonConvert.SerializeObject(obj), "application/json");
+    }
+
+    protected IActionResult InternalServerError(Exception ex)
+    {
+        return StatusCode(500, ex.Message);
     }
 }
