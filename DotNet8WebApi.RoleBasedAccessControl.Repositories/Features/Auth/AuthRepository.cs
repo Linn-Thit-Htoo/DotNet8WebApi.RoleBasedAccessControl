81ee5e0fd7660b8c6905a980548b229834f0bfd0
Linn Thit Htoo <lth1212001@gmail.com>
2024-07-08 23:23:11 +0630

    Format Code

--- DotNet8WebApi.RoleBasedAccessControl.Repositories/Features/Auth/AuthRepository.cs
+++ DotNet8WebApi.RoleBasedAccessControl.Repositories/Features/Auth/AuthRepository.cs
@@ -4,56 +4,54 @@ using DotNet8WebApi.RoleBasedAccessControl.Models.Features;
 using DotNet8WebApi.RoleBasedAccessControl.Models.Features.Auth;
 using DotNet8WebApi.RoleBasedAccessControl.Models.Resources;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace DotNet8WebApi.RoleBasedAccessControl.Repositories.Features.Auth
+namespace DotNet8WebApi.RoleBasedAccessControl.Repositories.Features.Auth;
+
+public class AuthRepository : IAuthRepository
 {
-    public class AuthRepository : IAuthRepository
+    private readonly AppDbContext _appDbContext;
+
+    public AuthRepository(AppDbContext appDbContext)
     {
-        private readonly AppDbContext _appDbContext;
+        _appDbContext = appDbContext;
+    }
 
-        public AuthRepository(AppDbContext appDbContext)
+    public async Task<Result<JwtResponseModel>> Login(LoginRequestModel requestModel)
+    {
+        Result<JwtResponseModel> responseModel;
+        try
         {
-            _appDbContext = appDbContext;
-        }
-
-        public async Task<Result<JwtResponseModel>> Login(LoginRequestModel requestModel)
-        {
-            Result<JwtResponseModel> responseModel;
-            try
+            var user = await _appDbContext.TblUsers.FirstOrDefaultAsync(x =>
+                x.Email == requestModel.Email && x.Password == requestModel.Password && x.IsActive
+            );
+            if (user is null)
             {
-                var user = await _appDbContext.TblUsers.FirstOrDefaultAsync(x => x.Email == requestModel.Email
-                && x.Password == requestModel.Password && x.IsActive);
-                if (user is null)
-                {
-                    responseModel = Result<JwtResponseModel>.FailureResult(MessageResource.NotFound, EnumStatusCode.NotFound);
-                    goto result;
-                }
-
-                var model = GetJwtResponseModel(user);
-                responseModel = Result<JwtResponseModel>.SuccessResult(model);
-            }
-            catch (Exception ex)
-            {
-                responseModel = Result<JwtResponseModel>.FailureResult(ex);
+                responseModel = Result<JwtResponseModel>.FailureResult(
+                    MessageResource.NotFound,
+                    EnumStatusCode.NotFound
+                );
+                goto result;
             }
 
-        result:
-            return responseModel;
+            var model = GetJwtResponseModel(user);
+            responseModel = Result<JwtResponseModel>.SuccessResult(model);
+        }
+        catch (Exception ex)
+        {
+            responseModel = Result<JwtResponseModel>.FailureResult(ex);
         }
 
-        private JwtResponseModel GetJwtResponseModel(TblUser user)
+    result:
+        return responseModel;
+    }
+
+    private JwtResponseModel GetJwtResponseModel(TblUser user)
+    {
+        return new JwtResponseModel
         {
-            return new JwtResponseModel
-            {
-                UserName = user.UserName,
-                Email = user.Email,
-                UserRole = user.UserRole
-            };
-        }
+            UserName = user.UserName,
+            Email = user.Email,
+            UserRole = user.UserRole
+        };
     }
 }
