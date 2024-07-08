98768665d7e7c3ab6adf59f92428b553756489b5
Linn Thit Htoo <lth1212001@gmail.com>
2024-07-08 23:21:59 +0630

    Format Code

--- DotNet8WebApi.RoleBasedAccessControl.Models/Features/Auth/JwtResponseModel.cs
+++ DotNet8WebApi.RoleBasedAccessControl.Models/Features/Auth/JwtResponseModel.cs
@@ -1,15 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace DotNet8WebApi.RoleBasedAccessControl.Models.Features.Auth;
 
-namespace DotNet8WebApi.RoleBasedAccessControl.Models.Features.Auth
+public class JwtResponseModel
 {
-    public class JwtResponseModel
-    {
-        public string UserName { get; set; }
-        public string Email { get; set; }
-        public string UserRole { get; set; }
-    }
+    public string UserName { get; set; }
+    public string Email { get; set; }
+    public string UserRole { get; set; }
 }
