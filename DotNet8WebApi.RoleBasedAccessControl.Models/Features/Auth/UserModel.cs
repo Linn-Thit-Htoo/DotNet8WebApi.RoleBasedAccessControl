c99fc0c1c3a9cc4ac3d3161107ccb1d8080484a6
Linn Thit Htoo <lth1212001@gmail.com>
2024-07-08 23:22:33 +0630

    Format Code

--- DotNet8WebApi.RoleBasedAccessControl.Models/Features/Auth/UserModel.cs
+++ DotNet8WebApi.RoleBasedAccessControl.Models/Features/Auth/UserModel.cs
@@ -1,23 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace DotNet8WebApi.RoleBasedAccessControl.Models.Features.Auth;
 
-namespace DotNet8WebApi.RoleBasedAccessControl.Models.Features.Auth
+public class UserModel
 {
-    public class UserModel
-    {
-        public int UserId { get; set; }
+    public int UserId { get; set; }
 
-        public string UserName { get; set; } = null!;
+    public string UserName { get; set; } = null!;
 
-        public string Email { get; set; } = null!;
+    public string Email { get; set; } = null!;
 
-        public string UserRole { get; set; } = null!;
+    public string UserRole { get; set; } = null!;
 
-        public string Password { get; set; } = null!;
+    public string Password { get; set; } = null!;
 
-        public bool IsActive { get; set; }
-    }
+    public bool IsActive { get; set; }
 }
