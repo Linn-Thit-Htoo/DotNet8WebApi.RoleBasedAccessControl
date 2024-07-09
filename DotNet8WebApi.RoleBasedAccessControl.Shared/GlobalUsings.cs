cc87b20af9823e49a05ae8724645bd977f2c77b0
Linn Thit Htoo <lth1212001@gmail.com>
2024-07-09 14:46:47 +0630

    [Complete]

--- DotNet8WebApi.RoleBasedAccessControl.Shared/GlobalUsings.cs
+++ DotNet8WebApi.RoleBasedAccessControl.Shared/GlobalUsings.cs
@@ -2,6 +2,7 @@
 
 global using System.IdentityModel.Tokens.Jwt;
 global using System.Security.Claims;
+global using System.Security.Cryptography;
 global using System.Text;
 global using Microsoft.Extensions.Configuration;
 global using Microsoft.IdentityModel.Tokens;
--- DotNet8WebApi.RoleBasedAccessControl.Shared/Services/AuthService/JWTAuth.cs
+++ DotNet8WebApi.RoleBasedAccessControl.Shared/Services/AuthService/JWTAuth.cs
@@ -1,55 +1,48 @@
-﻿using Microsoft.Extensions.Configuration;
-using Microsoft.IdentityModel.Tokens;
-using System;
-using System.Collections.Generic;
-using System.IdentityModel.Tokens.Jwt;
-using System.Linq;
-using System.Security.Claims;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace DotNet8WebApi.RoleBasedAccessControl.Shared.Services.AuthService;
 
-namespace DotNet8WebApi.RoleBasedAccessControl.Shared.Services.AuthService
+public class JWTAuth
 {
-    public class JWTAuth
+    private readonly IConfiguration _configuration;
+
+    public JWTAuth(IConfiguration configuration)
     {
-        private readonly IConfiguration _configuration;
+        _configuration = configuration;
+    }
 
-        public JWTAuth(IConfiguration configuration)
+    public string GetJWTToken(JwtResponseModel jwtResponseModel)
+    {
+        try
         {
-            _configuration = configuration;
-        }
-
-        public string GetJWTToken(JwtResponseModel jwtResponseModel)
-        {
-            try
+            var claims = new[]
             {
-                var claims = new[]
-                {
                 new Claim(JwtRegisteredClaimNames.Sub, _configuration["Jwt:Subject"]!),
                 new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
-                new Claim(JwtRegisteredClaimNames.Iat, ((DateTimeOffset)DateTime.UtcNow).ToUnixTimeSeconds().ToString(), ClaimValueTypes.Integer64),
+                new Claim(
+                    JwtRegisteredClaimNames.Iat,
+                    ((DateTimeOffset)DateTime.UtcNow).ToUnixTimeSeconds().ToString(),
+                    ClaimValueTypes.Integer64
+                ),
                 new Claim("UserName", jwtResponseModel.UserName),
                 new Claim("Email", jwtResponseModel.Email),
                 new Claim("UserRole", jwtResponseModel.UserRole),
                 new Claim(ClaimTypes.Role, jwtResponseModel.UserRole)
             };
 
-                var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]!));
-                var signIn = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
-                var token = new JwtSecurityToken(
-                    _configuration["Jwt:Issuer"],
-                    _configuration["Jwt:Audience"],
-                    claims,
-                    expires: DateTime.UtcNow.AddHours(10),
-                    signingCredentials: signIn
-                );
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]!));
+            var signIn = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+            var token = new JwtSecurityToken(
+                _configuration["Jwt:Issuer"],
+                _configuration["Jwt:Audience"],
+                claims,
+                expires: DateTime.UtcNow.AddHours(10),
+                signingCredentials: signIn
+            );
 
-                return new JwtSecurityTokenHandler().WriteToken(token);
-            }
-            catch (Exception ex)
-            {
-                throw ex;
-            }
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+        catch (Exception ex)
+        {
+            throw ex;
         }
     }
 }
