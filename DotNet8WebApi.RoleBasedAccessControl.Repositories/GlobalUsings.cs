d630236942b5f46729534e534127119049db3f35
Linn Thit Htoo <lth1212001@gmail.com>
2024-07-09 14:45:25 +0630

    Format Code

---
+++ DotNet8WebApi.RoleBasedAccessControl.Repositories/GlobalUsings.cs
@@ -0,0 +1,10 @@
+// Global using directives
+
+global using DotNet8WebApi.RoleBasedAccessControl.DbService.AppDbContexts;
+global using DotNet8WebApi.RoleBasedAccessControl.Models.Enums;
+global using DotNet8WebApi.RoleBasedAccessControl.Models.Features;
+global using DotNet8WebApi.RoleBasedAccessControl.Models.Features.Auth;
+global using DotNet8WebApi.RoleBasedAccessControl.Models.Resources;
+global using DotNet8WebApi.RoleBasedAccessControl.Shared.Services.AuthService;
+global using DotNet8WebApi.RoleBasedAccessControl.Shared.Services.SecurityServices;
+global using Microsoft.EntityFrameworkCore;
