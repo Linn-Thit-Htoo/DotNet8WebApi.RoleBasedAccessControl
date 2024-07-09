74a32715b6c385eb7e7257b026be3e4499e7729a
Linn Thit Htoo <lth1212001@gmail.com>
2024-07-09 14:40:13 +0630

    [Added] Required Files and implementations

--- DotNet8WebApi.RoleBasedAccessControl.Shared/DevCode.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DotNet8WebApi.RoleBasedAccessControl.Shared;
-
-public static class DevCode
-{
-    public static bool IsNullOrEmpty(this string str)
-    {
-        return string.IsNullOrEmpty(str) || string.IsNullOrWhiteSpace(str);
-    }
-}
