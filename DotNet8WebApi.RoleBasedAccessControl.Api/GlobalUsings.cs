fbcada2e4128a82a4dd831ab5b51193e76dccf3f
Linn Thit Htoo <lth1212001@gmail.com>
2024-07-09 14:41:06 +0630

    [Added] Global Usings

---
+++ DotNet8WebApi.RoleBasedAccessControl.Api/GlobalUsings.cs
@@ -0,0 +1,5 @@
+// Global using directives
+
+global using DotNet8WebApi.RoleBasedAccessControl.Models.Features.Auth;
+global using Microsoft.AspNetCore.Authorization;
+global using Microsoft.AspNetCore.Mvc;
