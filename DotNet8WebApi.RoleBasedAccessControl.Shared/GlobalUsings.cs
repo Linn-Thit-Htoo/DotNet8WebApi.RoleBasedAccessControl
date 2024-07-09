a6ac46ef0d0e12520f7caaa83bb94b316885d375
Linn Thit Htoo <lth1212001@gmail.com>
2024-07-09 14:46:46 +0630

    [Complete]

---
+++ DotNet8WebApi.RoleBasedAccessControl.Shared/GlobalUsings.cs
@@ -0,0 +1,7 @@
+// Global using directives
+
+global using System.IdentityModel.Tokens.Jwt;
+global using System.Security.Claims;
+global using System.Text;
+global using Microsoft.Extensions.Configuration;
+global using Microsoft.IdentityModel.Tokens;
